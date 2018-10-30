154dc9cb3b5a0f775bdd4a46176cb0cb1cbe8755
Stéphane Menut <smenut@dawan.fr>
2018-10-30 22:02:22 +0100

    correction diverse

--- src/app/component/notfound/notfound.component.ts
+++ src/app/component/notfound/notfound.component.ts
@@ -4,11 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-notfound',
   template: `
    <header>
-     <h2 class="text-center">404 - not found</h2>
+     <h2 class="text-center text-warning">404 - not found</h2>
      <p class="text-center"><a routerLink="/home">Retourner en cours</a></p>
-
-
-     <div style="width:100%;height:0;padding-bottom:35%;position:relative;"><iframe src="https://giphy.com/embed/z9E0VKpHX4kSI" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/harry-potter-mustaches-z9E0VKpHX4kSI"></a></p>
    </header>
   `,
   styles: []
